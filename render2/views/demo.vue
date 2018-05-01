4ca228238b2f42fa95a473296cb4edc11c48f061
ruochen.xie <ruochen.xie@bitmain.com>
2018-05-01 23:17:10 +0800

    first commit render

---
+++ render2/views/demo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Anchor :level="1" title="特性1">特性1</Anchor>
+        <Anchor :level="2" title="特性2">特性2</Anchor>
+        <Anchor :level="3" title="特性3">特性3</Anchor>
+    </div>
+</template>
+<script>
+    import Anchor from '../components/anchor.vue'
+    export default {
+        components: {Anchor},
+        props: {},
+        data () {
+            return {};
+        },
+        computed: {},
+        methods: {}
+    }
+</script>
