30079484c6cafbceab4fdc1bab63a2af52d9a1a3
ruochen.xie <ruochen.xie@bitmain.com>
2018-07-16 15:28:33 +0800

    update btm-coupon-demo-pkg 1.0.7; 尝试编译组件（失败）

---
+++ npm/btm-coupon-demo-pkg/README.md
@@ -0,0 +1,16 @@
+# btm-coupon-demo-pkg
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
---
+++ npm/btm-coupon-demo-pkg/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import VueI18n from 'vue-i18n'
+
+Vue.use(VueI18n)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
